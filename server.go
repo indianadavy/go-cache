c7c79924b742dcdb6c9ede0a09fd3ea31477fc1f
David Diaz <david.diaz@relativity.com>
2023-06-09 22:07:58 -0500

    init

---
+++ server.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type User struct {
+	ID       int
+	Username string
+}
+
+type Server struct {
+	db    map[int]*User
+	cache map[int]*User
+	dbhit int
+}
+
+func NewServer() *Server {
+	db := make(map[int]*User)
+
+	for i := 0; i < 100; i++ {
+		db[i+1] = &User{
+			ID:       i + 1,
+			Username: fmt.Sprintf("user_%d", i+1),
+		}
+	}
+	return &Server{
+		db:    db,
+		cache: make(map[int]*User),
+	}
+}
+
+func (s *Server) tryCache(id int) (*User, bool) {
+	user, ok := s.cache[id]
+	return user, ok
+}
+
+func (s *Server) handleGetUser(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, _ := strconv.Atoi(idStr)
+
+	// first try the cache
+	user, ok := s.tryCache(id)
+
+	if !ok {
+		// hit the database
+		user, ok = s.db[id]
+		if !ok {
+			panic("user not found")
+		}
+		s.dbhit++
+
+		// cache the user
+		s.cache[id] = user
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func main() {
+
+}
